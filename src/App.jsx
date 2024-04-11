a7615fc5bb0f298a8f480f5f56b7a8e642dd43ca
sina <sinaderik7@gmail.com>
2024-04-11 20:35:06 +0430

    feat: adding post feature added to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Counter from './features/counter/Counter'
 import Posts from '../src/components/Posts/features/Posts'
+import AddPostForm from './components/Posts/features/AddPostForm'
 
 
 export default function App() {
   return (
     <div >
       {/* <Counter /> */}
-    <Posts />
+      <AddPostForm />
+      <Posts />
     </div>
   )
 }
--- src/components/Posts/features/postsSlice.js
+++ src/components/Posts/features/postsSlice.js
@@ -18,9 +18,15 @@ const postSlice = createSlice({
     name: "posts",
     initialState,
     reducers: {
-
+        postAdded: (state, action) => {
+        //in react we cannot mutate the state like the code below and must use spread operator to added update the state
+        // but the reason the we are mutating the state as can be seen , is the redux uses Immer js under the hood 
+        // as a result we can write js code like this 
+            state.push(action.payload)
+        }
     }
 })
 
 export default postSlice.reducer
 export const selectAllPosts = (state) => state.posts
+export const { postAdded } = postSlice.actions
